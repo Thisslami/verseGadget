993e7724ed2a3ecdfdc8be22f69637cd8e2d1e0f
thisslami <akinyemioluwaseunjunior@gmail.com>
2025-02-16 13:51:45 +0100

    updated

--- BackEnd/utils/generateTokenAndSetCookie.js
+++ BackEnd/utils/generateTokenAndSetCookie.js
@@ -1,20 +1,37 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
+
+// const generateTokenAndSetCookie = (res, userId) => {
+ 
+//   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+//     expiresIn: "7d",
+    
+//   });
+
+
+//   res.cookie("token", token, {
+//     httpOnly: true,
+//     secure: true,
+//     //  process.env.NODE_ENV === "production",
+//     sameSite: "none",
+//     maxAge: 7 * 24 * 60 * 60 * 1000,
+//   });
+
+//   return token;
+// };
+// module.exports = { generateTokenAndSetCookie };
+
 
 const generateTokenAndSetCookie = (res, userId) => {
- 
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
-    
   });
-
 
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
   return token;
 };
-module.exports = { generateTokenAndSetCookie };
