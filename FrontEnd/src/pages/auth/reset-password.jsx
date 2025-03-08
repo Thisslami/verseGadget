15298689bfe2c2325e731ce6f2b4912be884a052
thisslami <akinyemioluwaseunjunior@gmail.com>
2025-03-08 19:34:16 +0100

    made new changes

--- FrontEnd/src/pages/auth/reset-password.jsx
+++ FrontEnd/src/pages/auth/reset-password.jsx
@@ -85,7 +85,7 @@ const ResetPasswordPage = () => {
       className="max-w-md w-full bg-gray-700 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
     >
       <div className="p-8">
-        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-black to-indigo-500 text-transparent bg-clip-text">
+        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-white to-white text-transparent bg-clip-text">
           Reset Password
         </h2>
         {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
